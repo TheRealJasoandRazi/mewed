59499c0916b729d2abfb990839adc36e24e97ccf
TheRealRaziandJaso <Neno701@outlook.com>
2023-10-11 12:01:41 +1100

    updated HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -29,7 +29,7 @@ const card = (
       }} 
       >
       <Typography sx={{ fontSize: 20}} color="text.primary" gutterBottom>
-        My Personal Projects, test
+        My Personal Projects
       </Typography>
       <Divider />
       <CardMedia
